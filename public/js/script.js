501e047acaadfa072ab13c7d8bd3c97ebbcd1d00
shridhar618 <141147285+shridhar618@users.noreply.github.com>
2025-06-29 20:23:57 +0530

    .

--- public/js/script.js
+++ public/js/script.js
@@ -43,12 +43,24 @@ function hire(){
     window.location.href="https://www.linkedin.com/in/shridhar-bhat-bb3410290/";
 }
 
-function navonmesh(){
-    window.location.href="#"
+function chatbot(){
+    window.location.href("https://flask-chatbot-3-pl35.onrender.com/")
 }
 
-function chatbot(){
-    window.location.href="https://www.linkedin.com/in/shridhar-bhat-bb3410290/";
+function navonmesh(){
+    window.location.href("#");
+}
+
+function chatapp(){
+    window.location.href("#")
+}
+
+function fitclub(){
+    window.location.href("#")
+}
+
+function todo(){
+    window.location.href("https://to-do-app-by-bolt-ai.vercel.app/")
 }
 
 
