ed60d9dd044bf395bf44e79401819f8e389472d0
shridhar618 <141147285+shridhar618@users.noreply.github.com>
2025-07-04 16:51:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,22 +28,3 @@ document.getElementById('logoutBtn').addEventListener('click', async function(e)
     }
 }); 
 
-function chatbot(){
-    window.location.href("https://flask-chatbot-3-pl35.onrender.com/");
-}
-
-function navonmesh(){
-    window.location.href("https://navomesh-main.vercel.app/");
-}
-
-function chatapp(){
-    window.location.href("#");
-}
-
-function fitclub(){
-    window.location.href("#");
-}
-
-function todo(){
-    window.location.href("https://to-do-app-by-bolt-ai.vercel.app/");
-}
