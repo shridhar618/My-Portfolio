0906da68c402b305c629a6b4f406d3683101c59b
shridhar618 <141147285+shridhar618@users.noreply.github.com>
2025-07-04 16:01:47 +0530

    .

--- js/script.js
+++ js/script.js
@@ -29,21 +29,21 @@ document.getElementById('logoutBtn').addEventListener('click', async function(e)
 }); 
 
 function chatbot(){
-    window.location.href("https://flask-chatbot-3-pl35.onrender.com/")
+    window.location.href("https://flask-chatbot-3-pl35.onrender.com/");
 }
 
 function navonmesh(){
-    window.location.href("#");
+    window.location.href("https://navomesh-main.vercel.app/");
 }
 
 function chatapp(){
-    window.location.href("#")
+    window.location.href("#");
 }
 
 function fitclub(){
-    window.location.href("#")
+    window.location.href("#");
 }
 
 function todo(){
-    window.location.href("https://to-do-app-by-bolt-ai.vercel.app/")
+    window.location.href("https://to-do-app-by-bolt-ai.vercel.app/");
 }
---
+++ public/project.js
@@ -0,0 +1,23 @@
+function hire(){
+    window.location.href="https://www.linkedin.com/in/shridhar-bhat-bb3410290/";
+}
+
+function chatbot(){
+    window.location.href("https://flask-chatbot-3-pl35.onrender.com/");
+}
+
+function navonmesh(){
+    window.location.href("https://navomesh-main.vercel.app/");
+}
+
+function chatapp(){
+    window.location.href("#");
+}
+
+function fitclub(){
+    window.location.href("#");
+}
+
+function todo(){
+    window.location.href("https://to-do-app-by-bolt-ai.vercel.app/");
+}
