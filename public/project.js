d34287f72274fec8e4dc14f577dfcf32becd242d
shridhar618 <141147285+shridhar618@users.noreply.github.com>
2025-08-02 17:37:25 +0530

    updated

--- public/project.js
+++ public/project.js
@@ -1,23 +1,27 @@
 function hire(){
-    window.location.href="https://www.linkedin.com/in/shridhar-bhat-bb3410290/";
+    window.location.href = "https://www.linkedin.com/in/shridhar-bhat-bb3410290/";
 }
 
 function chatbot(){
-    window.location.href="https://flask-chatbot-3-pl35.onrender.com/";
+    window.location.href = "https://flask-chatbot-3-pl35.onrender.com/";
 }
 
 function navonmesh(){
-    window.location.href="https://navomesh-main.vercel.app/";
+    window.location.href = "https://navomesh-main.vercel.app/";
 }
 
-function chatapp(){
-    window.location.href="#";
+function sitecrafter(){
+    window.location.href = "https://site-crafters.vercel.app/";
 }
 
 function fitclub(){
-    window.location.href="#";
+    window.location.href = "https://fitclub-gym-website.vercel.app/";
+}
+
+function notekeeper(){
+    window.location.href = "https://keeper-sepia-phi.vercel.app/";
 }
 
 function todo(){
-    window.location.href="https://to-do-app-by-bolt-ai.vercel.app/";
+    window.location.href = "https://to-do-app-by-bolt-ai.vercel.app/";
 }
